0b91b98f8182224c9d27249c149060b0a5e12603
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-08-03 08:22:44 +0900

    Update Candies.cpp

--- Candies.cpp
+++ Candies.cpp
@@ -1,1 +1,34 @@
+long candies(int n, vector<int> arr)
+{
+    vector<int> temp(n);
+    int num = 1;
+
+    for (int i = 1; i < n; ++i)
+    {
+        if (arr[i] > arr[i-1])
+            num++;
+        else if (arr[i] <= arr[i-1])
+             num = 1;
+        
+        temp[i] = num;
+    }
+
+    num = 1;
+    long count = temp[n-1];
+    
+    for (int i = n-2; i >= 0; --i)
+    {
+        if (arr[i] > arr[i+1])
+            num++;
+        else if (arr[i] <= arr[i+1])
+            num = 1;
+        
+        if( num > temp[i])
+            temp[i] = num;
+
+        count += temp[i];
+    }    
+    
+    return count;
+}
 
