b7132a238fb18277783070eb09fc0a8257af61cb
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-06-08 04:12:38 +0900

    Update Candies.cs

--- Candies.cs
+++ Candies.cs
@@ -30,3 +30,30 @@ class Result
     }
 
 }
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = new List<int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            int arrItem = Convert.ToInt32(Console.ReadLine().Trim());
+            arr.Add(arrItem);
+        }
+
+        long result = Result.candies(n, arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
