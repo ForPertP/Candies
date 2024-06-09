b232bb000b9b639b540b687d4644267d44d61a7e
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-06-10 04:54:18 +0900

    Create Candies.java

---
+++ Candies.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'candies' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static long candies(int n, List<Integer> arr) {
+    }
+
+}
